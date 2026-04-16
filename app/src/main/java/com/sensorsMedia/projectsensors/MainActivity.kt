13d29066b4c969adb87667f2741b75954418bd79
patorishia <ppatricia@ipvc.pt>
2026-04-16 20:11:09 +0100

    adicionar acelerómetro para mudar de musica com inclinação

--- app/src/main/java/com/sensorsMedia/projectsensors/MainActivity.kt
+++ app/src/main/java/com/sensorsMedia/projectsensors/MainActivity.kt
@@ -14,6 +14,14 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var sensorManager: SensorManager
     private var proximitySensor: Sensor? = null
+    private var accelerometer: Sensor? = null
+
+    private var currentSongIndex = 0
+    private val songs = listOf(
+        R.raw.musica1,
+        R.raw.musica2
+    )
+
 
 
 
@@ -25,7 +33,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
-
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         val startButton = findViewById<Button>(R.id.startButton)
         val statusText = findViewById<TextView>(R.id.statusText)
@@ -39,6 +47,13 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     override fun onResume() {
         super.onResume()
         proximitySensor?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+        accelerometer?.also {
             sensorManager.registerListener(
                 this,
                 it,
@@ -75,9 +90,46 @@ class MainActivity : ComponentActivity(), SensorEventListener {
                 }
             }
         }
+
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+
+            val x = event.values[0]
+
+            // Inclinação para a direita → próxima música
+            if (x > 5) {
+                nextSong()
+            }
+
+            // Inclinação para a esquerda → música anterior
+            if (x < -5) {
+                previousSong()
+            }
+        }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
+    private fun nextSong() {
+        currentSongIndex = (currentSongIndex + 1) % songs.size
+        changeSong()
+    }
+
+    private fun previousSong() {
+        currentSongIndex = if (currentSongIndex - 1 < 0) {
+            songs.size - 1
+        } else {
+            currentSongIndex - 1
+        }
+        changeSong()
+    }
+
+    private fun changeSong() {
+        mediaPlayer.reset()
+        mediaPlayer = MediaPlayer.create(this, songs[currentSongIndex])
+        mediaPlayer.start()
+    }
+
+
 }
+
 
