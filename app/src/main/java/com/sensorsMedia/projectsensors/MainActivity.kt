7c2bcecc137f3e0fd7bbba5f7d111a9c02b6bb42
patorishia <ppatricia@ipvc.pt>
2026-04-16 19:53:25 +0100

    adicionar sensor de proximidade para pausar e retomar música

--- app/src/main/java/com/sensorsMedia/projectsensors/MainActivity.kt
+++ app/src/main/java/com/sensorsMedia/projectsensors/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.sensorsMedia.projectsensors
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SensorEventListener {
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var sensorManager: SensorManager
+    private var proximitySensor: Sensor? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mediaPlayer = MediaPlayer.create(this, R.raw.musica1)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
 
         val startButton = findViewById<Button>(R.id.startButton)
         val statusText = findViewById<TextView>(R.id.statusText)
@@ -24,9 +36,48 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        proximitySensor?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+
     override fun onDestroy() {
         super.onDestroy()
         mediaPlayer.release()
     }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_PROXIMITY) {
+
+            val distance = event.values[0]
+
+            if (distance == 0f) {
+                // Objeto perto → PAUSAR
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.pause()
+                }
+            } else {
+                // Objeto longe → RETOMAR
+                if (!mediaPlayer.isPlaying) {
+                    mediaPlayer.start()
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
 }
 
